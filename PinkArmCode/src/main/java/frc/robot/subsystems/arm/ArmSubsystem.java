0f95b1139744f8bf7900f726a97d601e0be5e2cc
1withspaghetti <79611123+1withspaghetti@users.noreply.github.com>
2023-11-16 17:17:21 -0800

    Base Subsystems

---
+++ PinkArmCode/src/main/java/frc/robot/subsystems/arm/ArmSubsystem.java
@@ -0,0 +1,20 @@
+package frc.robot.subsystems.arm;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ArmSubsystem extends SubsystemBase {
+  /** Creates a new ExampleSubsystem. */
+  public ArmSubsystem() {
+    
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+}
---
+++ PinkArmCode/src/main/java/frc/robot/subsystems/elevator/ElevatorSubsystem.java
@@ -0,0 +1,21 @@
+package frc.robot.subsystems.elevator;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ElevatorSubsystem extends SubsystemBase {
+  /** Creates a new ElevatorSubsystem. */
+  public ElevatorSubsystem() {
+    
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+}
+
