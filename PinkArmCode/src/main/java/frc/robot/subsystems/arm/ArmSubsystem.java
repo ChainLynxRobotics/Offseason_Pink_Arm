b9aca3db9a758b08be6e4c6d3ef6a04565f96e9b
TurtleMeds <76972146+TurtleMeds@users.noreply.github.com>
2023-11-22 04:58:08 +0000

    added to constants and started on mech2d

--- PinkArmCode/src/main/java/frc/robot/subsystems/arm/ArmSubsystem.java
+++ PinkArmCode/src/main/java/frc/robot/subsystems/arm/ArmSubsystem.java
@@ -5,10 +5,23 @@ import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Robot;
 
-public class ArmSubsystem extends SubsystemBase {
+public class ArmSubsystem extends SubsystemBase implements AutoClosable {
+  @AutoLogOutput
+  private final Mechanism2d mechanism;
+  @AutoLogOutput
+  private final MechanismRoot2d root;
+  @AutoLogOutput
+  private final MechanismLigament2d ligament;
+  private final DCMotor m_eleatorGearbox = DCMotor.getNeo550(numMotors: 2);
+  private final CANSparkMax m_elevatorMotor = new CANSparkMax(Robot.Constants.kElevatorMotorPort, CANSparkMaxLowLevel.MotorType.kBrushless);
   /** Creates a new ArmSubsystem. */
   public ArmSubsystem() {
-    
+    mechanism = new Mechanism2d(
+    Constants.SIM_HEIGHT,
+    Constants.SIM_WIDTH
+    );
+    root = new MechanismRoot2d(mechanism.getRoot("arm", PIVOT_X_OFFSET, PIVOT_Y_OFFSET));
+    ligament = new MechanismLigament2d("ligament", 3, 0, 5, new Color8Bit(Color.kWhite));
   }
 
   @Override
@@ -20,7 +33,13 @@ public class ArmSubsystem extends SubsystemBase {
 
     }
   }
+  public void periodicSim() {
+    
+  }
 
+  public void setTarget(double length) {
+    
+  }
   public void setTargetPose(Pose3d targetPose, Pose2d currentPose) {
   }
 
@@ -30,5 +49,9 @@ public class ArmSubsystem extends SubsystemBase {
 
   private boolean checkValidState(double targetShoulderAngle, double targetExtensionLength) {
     throw new UnsupportedOperationException("unimplemented");
+  }
+  @Override
+  public void close() throws Exception {
+    mech2d.close();
   }
 }
