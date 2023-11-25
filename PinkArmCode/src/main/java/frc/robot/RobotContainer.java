506b94a4e126e27f1dfcfc953c0eb48f3b07d1e1
Irishumanoid <95321751+Irishumanoid@users.noreply.github.com>
2023-11-25 13:27:56 -0800

    added armiosparkmax to robot container

--- PinkArmCode/src/main/java/frc/robot/RobotContainer.java
+++ PinkArmCode/src/main/java/frc/robot/RobotContainer.java
@@ -9,6 +9,7 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.commands.ArmSimCommand;
 import frc.robot.subsystems.arm.ArmIOSim;
+import frc.robot.subsystems.arm.ArmIOSparkMax;
 import frc.robot.subsystems.arm.ArmSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.PrintCommand;
