fc675961bf6bcc0077ac312b5370e067601b3152
TurtleMeds <owenwells2009@gmail.com>
2023-11-28 16:55:53 -0800

    modified:   PinkArmCode/src/main/java/frc/robot/subsystems/arm/ArmSubsystem.java

--- PinkArmCode/src/main/java/frc/robot/subsystems/arm/ArmSubsystem.java
+++ PinkArmCode/src/main/java/frc/robot/subsystems/arm/ArmSubsystem.java
@@ -4,6 +4,8 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Robot;
+import frc.robot.Constants.ArmConstants;
+import 
 
 public class ArmSubsystem extends SubsystemBase implements AutoClosable {
   @AutoLogOutput
@@ -12,15 +14,15 @@ public class ArmSubsystem extends SubsystemBase implements AutoClosable {
   private final MechanismRoot2d root;
   @AutoLogOutput
   private final MechanismLigament2d ligament;
-  private final DCMotor m_eleatorGearbox = DCMotor.getNeo550(numMotors: 2);
-  private final CANSparkMax m_elevatorMotor = new CANSparkMax(Robot.Constants.kElevatorMotorPort, CANSparkMaxLowLevel.MotorType.kBrushless);
+  private final DCMotor m_eleatorGearbox = DCMotor.getNeo550(numMotors 2);
+  private final CANSparkMax m_elevatorMotor = new CANSparkMax(ArmConstants.controller1port, CANSparkMaxLowLevel.MotorType.kBrushless);
   /** Creates a new ArmSubsystem. */
   public ArmSubsystem() {
     mechanism = new Mechanism2d(
-    Constants.SIM_HEIGHT,
-    Constants.SIM_WIDTH
+    ArmConstants.SIM_HEIGHT,
+    ArmConstants.SIM_WIDTH
     );
-    root = new MechanismRoot2d(mechanism.getRoot("arm", PIVOT_X_OFFSET, PIVOT_Y_OFFSET));
+    root = new MechanismRoot2d(mechanism.getRoot("arm", ArmConstants.PIVOT_X_OFFSET, ArmConstants.PIVOT_Y_OFFSET));
     ligament = new MechanismLigament2d("ligament", 3, 0, 5, new Color8Bit(Color.kWhite));
   }
 
@@ -45,7 +47,7 @@ public class ArmSubsystem extends SubsystemBase implements AutoClosable {
 
   private Pose2d calcCurrentPose(double armLengthMeters, double shoulderAngleRad) {
     throw new UnsupportedOperationException("unimplemented");
-  }0
+  }
 
   private boolean checkValidState(double targetShoulderAngle, double targetExtensionLength) {
     throw new UnsupportedOperationException("unimplemented");
